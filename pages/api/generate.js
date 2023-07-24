4b592563cc02002eafb5fe2ce4c6ffaa9c20db6a
B-Abbas <35643870+B-Abbas@users.noreply.github.com>
2023-07-24 23:22:06 +0100

    Update generate.js with comments from chatGPT itself

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,11 +1,17 @@
+// Import necessary dependencies from the "openai" module.
 import { Configuration, OpenAIApi } from "openai";
 
+// Create a new instance of the Configuration class with the OpenAI API key from the environment variable.
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+// Create a new instance of the OpenAIApi class, passing the configuration object with the API key.
 const openai = new OpenAIApi(configuration);
 
+// Define the main request handler function that will be executed when the server receives a POST request.
 export default async function (req, res) {
+  // Check if the OpenAI API key is missing or not configured.
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -15,7 +21,10 @@ export default async function (req, res) {
     return;
   }
 
+  // Retrieve the value of the "animal" property from the request body, or assign an empty string if not present.
   const animal = req.body.animal || '';
+
+  // Check if the "animal" parameter is missing, empty, or contains only whitespace characters.
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
@@ -26,18 +35,24 @@ export default async function (req, res) {
   }
 
   try {
+    // Make an API call to OpenAI using the createCompletion method.
+    // Provide the GPT-3 model ("text-davinci-003"), the generated prompt with the animal name, and set the temperature to 0.6.
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
+
+    // Respond with a 200 status and send back the suggested superhero names extracted from the OpenAI API response.
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+    // If an error occurs during the API call, handle the error and respond accordingly.
     if (error.response) {
+      // Log the status and data of the API response and send back the error response with the same status and data.
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
     } else {
+      // If there is no response from the OpenAI API, log the error message and send back a 500 status response with a generic error message.
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
@@ -48,9 +63,13 @@ export default async function (req, res) {
   }
 }
 
+// Define the generatePrompt function that takes an "animal" parameter and generates a prompt for the OpenAI API.
 function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  // Capitalize the first letter of the animal's name for consistency.
+  const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  
+  // Create a template prompt asking the AI to suggest three superhero names for the provided animal.
+  // Provide examples for "Cat" and "Dog" superheroes along with their names, and include the capitalized animal name in the prompt.
   return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
